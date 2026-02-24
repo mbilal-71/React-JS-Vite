e6fd5a80fddb96f56db149f652af5d8a49154c06
Muhammad Bilal <mb16837157@gmail.com>
2026-02-24 08:29:20 +0500

    react

--- React app/src/components/Card.jsx
+++ React app/src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Bookmark from 'lucide-react'
+import { FaBookmark } from 'react-icons/fa';
 const Card = () => {
   return (
    
       <div className="card1">
       <div className="top">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFkkQhwA-14yQus_rij6VonTP5UT7IrorBkg&s" alt="" />
-        <button>Save 
+        <button>Save  <FaBookmark /> 
         </button>
        
       </div>
