0bdc1fdc8334bd8b32d7d0a4500d31d82bf065d0
Muhammad Bilal <mb16837157@gmail.com>
2026-03-06 22:02:06 +0500

    react

--- React app/src/components/Card1.jsx
+++ React app/src/components/Card1.jsx
@@ -12,6 +12,7 @@ const Card1 = (props) => {
       <h1>{props.user}</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       <button>View Profile</button>
+      <button>View Profile</button>
     </div>
 
   );
