d8f6450fe2efca95412050875681d2ebfc5a42dc
Muhammad Bilal <mb16837157@gmail.com>
2026-05-23 14:37:41 +0500

    react

--- React app/src/components/Card1.jsx
+++ React app/src/components/Card1.jsx
@@ -9,9 +9,6 @@ const Card1 = (props) => {
         src="https://images.unsplash.com/photo-1771655393383-aeded39730fa?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5fHx8ZW58MHx8fHx8"
         alt="" />
       {/* <h1>UNSPLASH</h1> */}
-      <h1>{props.user}</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      <button>View Profile</button>
     </div>
   );
 };
