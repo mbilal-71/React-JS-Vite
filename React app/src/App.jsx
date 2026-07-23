4c953897f4d90d9f0005635265dbc34d36175b9b
Muhammad Bilal <mb16837157@gmail.com>
2026-07-23 21:19:29 +0500

    react

--- React app/src/App.jsx
+++ React app/src/App.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import Card1 from './components/Card1';
 import Card from './components/Card';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Card1 user="ali" age={80} />
-      <Card1 />
-      <Card/>
-      <Navbar />
-      <Footer />
+      <Card />
     </>
   );
 }
